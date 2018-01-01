ff6de61cdb217c9542dc0ea6946b113902b7bfec
awmleer <awmleer@sparker.xyz>
2018-01-01 22:24:09 +0800

    add rom and ram delay

--- src/data_ram.v
+++ src/data_ram.v
@@ -1,7 +1,7 @@
 module data_ram (
 	output reg ram_stall,
 	input wire rst,
-	input wire cs,	
+	input wire cs,
 
 	input wire clk,
 	input wire we,
@@ -9,31 +9,53 @@ module data_ram (
 	input wire [31:0] din,
 	output reg [31:0] dout
 	);
-	
+
 	parameter
 		ADDR_WIDTH = 5;
-	
+
 	reg [31:0] data [0:(1<<ADDR_WIDTH)-1];
-	
+    reg [3:0]counter;
+    reg [31:0] addr_previous;
+    reg ack;
+
 	initial	begin
 		$readmemh("data_mem.hex", data);
 	end
-	
+
 	always @(negedge clk) begin
 		if (we && addr[31:ADDR_WIDTH]==0)
 			data[addr[ADDR_WIDTH-1:0]] <= din;
 	end
-	
+
 	reg [31:0] out;
 	always @(negedge clk) begin
-		out <= data[addr[ADDR_WIDTH-1:0]];
+        if (rst) begin
+            counter=0;
+            ack=0;
+        end else begin
+            if (addr_previous==addr) begin
+                counter+=1;
+                if (counter==7) begin
+                    if (we && addr[31:ADDR_WIDTH]==0)
+                        data[addr[ADDR_WIDTH-1:0]] = din;
+                    out = data[addr[ADDR_WIDTH-1:0]];
+                    ack=1;
+                end
+            end else begin
+                counter=0;
+                ack=0;
+            end
+            addr_previous=addr;
+        end
 	end
-	
+
+    assign ram_stall = cs & ~ack;
+
 	always @(*) begin
 		if (addr[31:ADDR_WIDTH] != 0)
 			dout = 32'h0;
 		else
 			dout = out;
 	end
-	
+
 endmodule
--- src/inst_rom.v
+++ src/inst_rom.v
@@ -1,32 +1,52 @@
 module inst_rom (
-	output reg rom_stall,
+	output wire rom_stall,
 	input wire rst,
-	input wire cs,	
+	input wire cs,
 
 	input wire clk,
 	input wire [31:0] addr,
 	output reg [31:0] dout
 	);
-	
+
 	parameter
 		ADDR_WIDTH = 6;
-	
+
 	reg [31:0] data [0:(1<<ADDR_WIDTH)-1];
-	
+
 	initial	begin
 		$readmemh("inst_mem.hex", data);
 	end
-	
+
 	reg [31:0] out;
+    reg [3:0]counter;
+    reg [31:0] addr_previous;
+    reg ack;
 	always @(negedge clk) begin
-		out <= data[addr[ADDR_WIDTH-1:0]];
+        if (rst) begin
+            counter=0;
+            ack = 0;
+        end else begin
+            if (addr_previous==addr) begin
+                counter+=1;
+                if (counter==7) begin
+                    out <= data[addr[ADDR_WIDTH-1:0]];
+                    ack=1;
+                end
+            end else begin
+                counter=0;
+                ack=0;
+            end
+            addr_previous=addr;
+        end
 	end
-	
+
+    assign rom_stall = cs & ~ack;
+
 	always @(*) begin
 		if (addr[31:ADDR_WIDTH] != 0)
 			dout = 32'h0;
 		else
 			dout = out;
 	end
-	
+
 endmodule
